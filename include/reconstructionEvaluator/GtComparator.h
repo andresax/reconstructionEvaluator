d0b12a36f1eb64a66531d96e6092ce049f449f6d
Andrea Romanoni <andrearomanoni@gmail.com>
2016-12-13 17:04:24 +0100

    main class added

---
+++ include/reconstructionEvaluator/GtComparator.h
@@ -0,0 +1,21 @@
+/*
+ * GtComparator.h
+ *
+ *  Created on: Dec 13, 2016
+ *      Author: andrea
+ */
+
+#ifndef SRC_GTCOMPARATOR_H_
+#define SRC_GTCOMPARATOR_H_
+
+namespace reconstructorEvaluator {
+
+class GtComparator {
+public:
+  GtComparator();
+  virtual ~GtComparator();
+};
+
+} /* namespace reconstructorEvaluator */
+
+#endif /* SRC_GTCOMPARATOR_H_ */
---
+++ src/GtComparator.cpp
@@ -0,0 +1,21 @@
+/*
+ * GtComparator.cpp
+ *
+ *  Created on: Dec 13, 2016
+ *      Author: andrea
+ */
+
+#include "GtComparator.h"
+
+namespace reconstructorEvaluator {
+
+GtComparator::GtComparator() {
+  // TODO Auto-generated constructor stub
+
+}
+
+GtComparator::~GtComparator() {
+  // TODO Auto-generated destructor stub
+}
+
+} /* namespace reconstructorEvaluator */
